d2cfa611439ef7997f1d69c9bc02589e52eb9653
kenankarakecili <kenankarakecili@icloud.com>
2016-04-18 14:44:47 +0300

    Initial Commit

---
+++ PickerView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PickerView
+//
+//  Created by Kenan Karakecili on 18/04/16.
+//  Copyright © 2016 Kenan Karakecili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
